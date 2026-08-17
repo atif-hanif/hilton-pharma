6709c197206db624c1a6475414497eb1c5069a69
atif-hanif <94978139+atif-hanif@users.noreply.github.com>
2026-08-17 07:41:39 +0500

    team master, roles master, user roles and admin employee pages updated

---
+++ assets/js/team-master.js
@@ -0,0 +1,19 @@
+new DataTable('#team_management', {
+	pageLength: 10,
+	ordering: true,
+	searching: true,
+	info: true,
+	responsive: true,
+	destroy: true,
+	paging: true,
+	dom:
+	'<"row align-items-center mb-3"' +
+	'<"col-md-6"l>' +
+	'<"col-md-6 d-flex justify-content-end"f>' +
+	'>' +
+	'rt' +
+	'<"row mt-3 align-items-center"' +
+	'<"col-md-6"i>' +
+	'<"col-md-6 d-flex justify-content-end"p>' +
+	'>',
+});
